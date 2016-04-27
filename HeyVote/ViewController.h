4ae9082dc69d3193ad68cddca1f4affbfe5af287
Ikhram Khan <ikhramkhan@ikhrams-macbook-pro.local>
2016-04-27 06:27:21 +0530

    Initial Commit

---
+++ HeyVote/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HeyVote
+//
+//  Created by Ikhram Khan on 4/27/16.
+//  Copyright © 2016 AppCandles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HeyVote/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HeyVote
+//
+//  Created by Ikhram Khan on 4/27/16.
+//  Copyright © 2016 AppCandles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
